651d9d81e6ca8346fbcbb05bb7dea5e18dd6f23c
tappetar <145151180+tappetar@users.noreply.github.com>
2024-06-24 11:46:40 -0400

    Add files via upload

--- gen4code.ino
+++ gen4code.ino
@@ -1,14 +1,13 @@
-
-#include "Arduino.h"
-#include "co2measure.h"
+#include "Arduino.h" // DOWNLOAD THIS YOURSELF
+#include "co2measure.h" 
 #include "pidfunction.h"
 #include "MFCSerial.h"
-#include "Wire.h"
-#include "SparkFun_STC3x_Arduino_Library.h"  //Click here to get the library: http://librarymanager/All#SparkFun_STC3x
-#include "SoftwareSerial.h"
+#include "Wire.h" // DOWNLOAD THIS YOURSELF
+#include "SparkFun_STC3x_Arduino_Library.h"  /// DOWNLOAD THIS YOURSELF
+#include "SoftwareSerial.h" // DOWNLOAD THIS YOURSELF
 
-SoftwareSerial sws2(10, 11);
-SoftwareSerial sws1(12, 13);
+// SoftwareSerial sws2(10, 11);
+// SoftwareSerial sws1(12, 13);
 
 
 
@@ -17,8 +16,8 @@ MFCSerial mfc1(&Serial1);
 
 
 //STC3x CO2Sensor;
-COZIR sensor1(&sws1);
-COZIR sensor2(&Serial2);
+//COZIR sensor1(&sws1);
+// COZIR sensor2(&Serial2);
 
 #define START_DELIM '['
 #define END_DELIM ']'
@@ -29,56 +28,48 @@ String toChange;
 
 char receivedChars[numChars];
 //char toChange[numChars];
-char tempChars[numChars]; 
+char tempChars[numChars];
 
 int tegco2 = 20;
-int time = 0;
+long loops = 0;
+long elapsed = 0;
 float Kp = 0.1;
-float Ki = 0.00001;
-float Kd = 0.1;
+float Ki = 0.000002;
+float Kd = 0;  //tuning
 float currCO2a;
 float currCO2b;
 float currCO2c;
-float sweep2 = 0.5;  //CHANGE VALUE FOR FIXED SWEEP
+float sweep2 = 5;  //CHANGE VALUE FOR FIXED SWEEP
 float dataNumber = 0.0;
 float changeVal;
+float fixed = 0.0;
 bool newData = false;
 bool looped = false;
+bool fixedSweep = false;
+String unitID = "A";
+
 
 void setup() {
 
-  Serial.begin(9600);
-  sws1.begin(9600);
-  sws2.begin(9600);
-
-  //Wire.begin();
-
-  //tegco2 = tegco2/0.00076;
-  sensor1.CO2setup();
-  //sensor2.CO2setup();
   PIDsetup();
-  mfc1.setupFlow(9600, 5.0, "A");
-
+  Serial.begin(9600);
   Serial1.begin(9600);
   Serial2.begin(9600);
+
 }
 
 void loop() {
-
   while (true) {
 
-    recvWithStartEndMarkers(); 
+    recvWithStartEndMarkers();
     if (newData == true) {
-        strcpy(tempChars, receivedChars);
-            // this temporary copy is necessary to protect the original data
-            //   because strtok() used in parseData() replaces the commas with \0
-        parseData();
-        newData = false;
+      strcpy(tempChars, receivedChars);
+      parseData();
+      newData = false;
     }
 
 
     if (Serial1.available() > 0) {
-
 
       recvWithEndMarker(&Serial1);
       showNewNumber();
@@ -86,54 +77,34 @@ void loop() {
       if (looped) {
         continue;
       }
-      // float prevCO2 = currCO2a;
-      // currCO2a = sensor1.CO2loop();
-      // currCO2a = currCO2a * 0.00076;
-
-      // if (currCO2a < 0) {
-      //   continue;
-      // }
-      // Serial.print((String)currCO2a);
-
-
-
-
-      // Serial.print(",");
-
-
-
-      // currCO2b = sensor2.CO2loop();
-      // currCO2b = currCO2b * 0.00076;
-
-      // //currCO2 = currCO2 * 1000 * 0.00000076;
-      // // if(abs(prevCO2-currCO2 > 3)){
-      // //   Serial.print("C");
-      // //   continue;
-      // // }
-      // // //Serial.print("currCO2 -> ");
-      // if (currCO2b < 0) {
-      //   continue;
-      // }
-      // Serial.print((String)currCO2b);
-
-      // Serial.print(",");
 
       sweep2 = PIDloop(currCO2c, tegco2, Kp, Ki, Kd);
+      float temp = 0;
+      if(loops % 900 == 899){
+        temp = mfc1.getFlow();
+        mfc1.setFlow(2);
+        delay(500);
+        mfc1.setFlow(temp);
+        sweep2 = 20;
+      }
+      
+      if (fixedSweep) {
+        sweep2 = fixed;
+      }
       Serial.print("[sweep=");
       Serial.print((String)sweep2);
       Serial.print("]");
-      Serial.print("[time=");
-      Serial.print((String)time);
+      Serial.print("[loops=");
+      Serial.print((String)loops);
+      Serial.print("]");
+      loops++;
+      elapsed = millis()/1000;
+      Serial.print("[elapsed=");
+      Serial.print((String)elapsed);
       Serial.println("]");
-      time++;
       mfc1.setFlow(sweep2 / 10);
-
       looped = true;
     }
-
-    // BELOW IS FIXED SWEEP (UNCOMMENT FOR FIXED SWEEP BUT COMMENT OUT ABOVE)
-
-    //mfc1.setFlow(sweep/10);
   }
 }
 
@@ -211,27 +182,43 @@ void parseData() {  // split the data into its parts
   char *strtokIndx;  // this is used by strtok() as an index
 
   strtokIndx = strtok(tempChars, "=");  // get the first part - the string
-  strcpy(tempChars, strtokIndx);         // copy it to messageFromPC
+  strcpy(tempChars, strtokIndx);        // copy it to messageFromPC
 
   strtokIndx = strtok(NULL, ",");  // this continues where the previous call left off
   changeVal = atof(strtokIndx);    // convert this part to an integer
 
   String abc(tempChars);
-  if(abc == "tEGCO2"){
+
+  if (abc == "tEGCO2") {
     tegco2 = changeVal;
+  } else if (abc == "sweep") {
+    fixed = changeVal;
+  } else if (abc == "fixed") {
+    if (changeVal == 1) {
+      fixedSweep = true;
+    } else if (changeVal == 0) {
+      fixedSweep = false;
+    }
+  } else if (abc == "clear"){
+      blast();
   }
   // strtokIndx = strtok(NULL, ",");
   // floatFromPC = atof(strtokIndx);  // convert this part to a float
 }
 
 
-
+void blast(){
+  float temp = mfc1.getFlow();
+  mfc1.setFlow(2);
+  delay(1000);
+  mfc1.setFlow(temp);
+}
 
 void showNewNumber() {
   if (newData == true) {
     dataNumber = 0.000;
     dataNumber = atof(receivedChars);
-    dataNumber = dataNumber * 7.6;
+    dataNumber = dataNumber * 7.6;  // THIS IS THE VALUE TO FILTER 
     currCO2c = dataNumber;
     Serial.print("[EGCO2=");
     Serial.print(dataNumber);
@@ -239,6 +226,7 @@ void showNewNumber() {
     Serial.print("[tEGCO2=");
     Serial.print(tegco2);
     Serial.print("]");
+    //Serial.println(dataNumber);
     newData = false;
     looped = false;
   }
