92d0147add488ee3942a6a28cecc75c637c81388
azhang / meepslive <andrewzhang100@gmail.com>
2024-12-13 18:11:25 -0500

    (hopefully) harmless comments

--- gen4code/gen4code.ino
+++ gen4code/gen4code.ino
@@ -15,7 +15,7 @@
 
 
 //MFCSerial mfc1(&Serial1);
-SMP mfc1(&Serial2, 1, 9600); // extends off serial port 1
+SMP mfc1(&Serial2, 1, 9600);  // extends off serial port 1
 SMP CO2Sensor(&Serial2, 4, 9600);
 COZIR sensor1(&Serial2);
 
@@ -30,7 +30,7 @@ String toChange;
 
 char receivedChars[numChars];
 //char toChange[numChars];
-char tempChars[numChars];
+char tempChars[numChars]; // TODO Andy: Can we make this a local variable in the parse() function?
 
 int tegco2 = 48;
 int prev = 0;
@@ -44,7 +44,7 @@ float currCO2b;
 float currCO2c;
 float sweep2 = 5;  //CHANGE VALUE FOR FIXED SWEEP
 float dataNumber = 0.0;
-float changeVal;
+float changeVal; // TODO Andy: Can we make this a local variable in the parse() function?
 float fixed = 0.0;
 bool newData = false;
 bool looped = false;
@@ -62,21 +62,19 @@ void setup() {
   Serial1.begin(9600);
   Serial2.begin(9600);
   Serial2.print("Serial2 online");
-  mfc1.switchPort(2); //multiplexer port 8
+  mfc1.switchPort(2);  //multiplexer port 8
   sensor1.CO2setup();
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
-  //Serial.print("blah blah blah this stoopid");
- //\ setrtc();
-  //digitalWrite(3, HIGH);
-  //Serial.print("this sucks!!!!!");
+
+  //  TODO Andy: Write out how Serial is connected here please
 }
 
 void loop() {
   while (true) {
     CO2Sensor.switchPort(4);
-    double receivedCO2 = (sensor1.CO2loop() * 0.00076 * 12.38) - 1.9 + 1.5 - 35-47; // calibrated value
+    double receivedCO2 = (sensor1.CO2loop() * 0.00076 * 12.38) - 1.9 + 1.5 - 35 - 47;  // calibrated value
     recvSerial();
     recvSerial3();
 
@@ -86,9 +84,7 @@ void loop() {
       newData = false;
     }
 
-    if (Serial1.available() > 0) {
-
-
+    if (Serial1.available() > 0) {  // TODO Andy: What is the purpose of this if-statement?
 
       recvWithEndMarker(&Serial1);
       showNewNumber();
@@ -108,19 +104,19 @@ void loop() {
       if (fixedSweep) {
         sweep2 = fixed;
       }
-      
+
       sendCommand("tSweep", (String)sweep2);
       sendCommand("loops", (String)loops);
       loops++;
       elapsed = millis() / 1000;
       sendCommand("elapsed", (String)elapsed);
-      if(loops % 1800 == 1799){
+      if (loops % 1800 == 1799) {
         blast();
-      } //every 30 mins
+      }  //every 30 mins
       mfc1.setFlow(sweep2 / 10);
       delay(200);
       looped = true;
-      
+
       //Serial.println();
       Serial3.println();
       Serial.println();
@@ -130,7 +126,7 @@ void loop() {
 }
 
 
-
+// TODO Andy: What is the purpose of recvWithEndMarker? Nothing is connected to Serial1 RX?
 float recvWithEndMarker(HardwareSerial *port) {
   static byte ndx = 0;
   char endMarker = '\n';
@@ -155,8 +151,15 @@ float recvWithEndMarker(HardwareSerial *port) {
   return 0;
 }
 
-
-
+// TODO Andy: receivedChars is a global variable used by both recvSerial and recvSerial3
+//            if you get a message on both Serial and Serial3 simultanously (one starts before the other ends)
+//            receivedChars will be corrupted
+//            That being said, this is not really that big a problem because chances are
+//            only one channel will be used as input at a time
+//            It's also very unlikely given that the messages are typically send in a fraction of a second
+//            Code would be more "elegant" if the message were processed entirely in the function,
+//            or the entire command once it got the endMarker. And then fed directly into the parse() function
+//            That would reduce the number of global variables and flags.
 void recvSerial() {
   static boolean recvInProgress = false;
   static byte ndx = 0;
@@ -164,7 +167,7 @@ void recvSerial() {
   char endMarker = ']';
   char rc;
 
-  while (Serial.available() > 0 &&  newData == false) {
+  while (Serial.available() > 0 && newData == false) {
     rc = Serial.read();
 
     if (recvInProgress == true) {
@@ -186,12 +189,7 @@ void recvSerial() {
       recvInProgress = true;
     }
   }
-
-
-
-
 }
-
 
 void recvSerial3() {
   static boolean recvInProgress = false;
@@ -222,10 +220,6 @@ void recvSerial3() {
       recvInProgress = true;
     }
   }
-
-
-
-
 }
 
 
@@ -268,8 +262,6 @@ void parseData() {  // split the data into its parts
     Serial.println("");
   }
 
-
-
   // strtokIndx = strtok(NULL, ",");
   // floatFromPC = atof(strtokIndx);  // convert this part to a float
 }
@@ -286,13 +278,11 @@ void showNewNumber() {
   if (newData == true) {
     dataNumber = 0.000;
     dataNumber = atof(receivedChars);
-    dataNumber = dataNumber * 7.6;  // THIS IS THE VALUE TO FILTER
-    currCO2c = dataNumber + 9 + 71;     //calibrate with room air
+    dataNumber = dataNumber * 7.6;   // THIS IS THE VALUE TO FILTER
+    currCO2c = dataNumber + 9 + 71;  //calibrate with room air
     //printtime();
     sendCommand("altEgco2", (String)currCO2c);
     sendCommand("tegco2", (String)tegco2);
-
-
 
     newData = false;
     looped = false;
